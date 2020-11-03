0201fec567d8fded6c9f514d1333b5b6afacbf68
Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
2020-11-02 16:51:04 -0800

    build_new_python_recipes.py: Script to build newly upgraded Yocto python recipes
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>

---
+++ yocto-scripts/build_new_python_recipes.py
@@ -0,0 +1,25 @@
+import glob
+import os
+import shutil
+
+basepath = "/workspaces/ckalluri/yocto/Xilinx_master"
+recipepath = os.path.join(basepath,"sources/meta-jupyter/recipes-python")
+buildpath = os.path.join(basepath,'builds-upgrade')
+
+
+shell_cmd='git status | cut -d ">" -f 2 | tail -n +6 | head -n -13  > upgrade_file_list.log'
+os.system(shell_cmd)
+
+pkgs = [line.strip().split('_')[0] for line in open('upgrade_file_list.log')]
+
+
+data='MACHINE="zcu102-zynqmp"'
+
+with open(os.path.join(basepath,'conf/local.conf'),'a') as f:
+        f.write(data)
+
+shutil.copyfile("/workspaces/ckalluri/yocto/Xilinx_master/build/conf/bblayers.conf", "/workspaces/ckalluri/yocto/Xilinx_master/builds-upgrade/conf/bblayers.conf")
+for pkg in pkgs:
+	os.chdir(basepath)
+	os.system('source sources/core/oe-init-build-env builds-upgrade  >& /dev/null;' + 'bitbake %s | tee %s_log.txt'%(pkg,pkg))
+
