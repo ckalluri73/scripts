58ee8921b4c52f29efff9f70fb552933befb394a
Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>
2020-11-16 13:14:33 -0800

    freshproject.sh: Fix argument passed to the script
    Fix arguments and git repo URL correctly for the script to be functional
    
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>

--- yocto-scripts/freshproject.sh
+++ yocto-scripts/freshproject.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 MANIFEST_BRANCH=$1
-REPO_BRANCH=rel-$1
+REPO_BRANCH=$1
 XML_FILE=default.xml
 
 /usr/bin/curl -k https://storage.googleapis.com/git-repo-downloads/repo > repo
 chmod a+x repo
-./repo init -u git://gitenterprise/Yocto/yocto-manifests.git -b $MANIFEST_BRANCH -m $XML_FILE
-#./repo init -u git://gitenterprise.xilinx.com/jaewon/yocto-manifests-zynq3.git -b master -m $XML_FILE
+./repo init -u https://gitenterprise.xilinx.com/Yocto/yocto-manifests.git -b $MANIFEST_BRANCH -m $XML_FILE
 ./repo sync -j 20
 ./repo start $REPO_BRANCH --all
